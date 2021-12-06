bdbf97f57b1db784d628ac3ea1f320cc5c0572a4
sohee7528 <sohee7528@hanmail.net>
2021-12-06 11:41:08 +0900

    프래그먼트 생명주기 로그추가

--- app/src/main/java/com/android/mobileprogramming14/Fragment1.java
+++ app/src/main/java/com/android/mobileprogramming14/Fragment1.java
@@ -1,10 +1,14 @@
 package com.android.mobileprogramming14;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +21,68 @@ public class Fragment1 extends Fragment {
     Button btnInput;
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        Log.i("life-F","onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i("life-F","onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i("life-F","onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("life-F","onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i("life-F","onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i("life-F","onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i("life-F","onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("life-F","onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i("life-F","onDetach");
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_1, container, false);
+
+        Log.i("life-F","onCreateView");
 
         edtInput = rootView.findViewById(R.id.edtInput);
         btnInput = rootView.findViewById(R.id.btnInput);
